8bdde7249201270f0818be77b5fffb447b87882c
Administrator <Administrator@SC-201903102354>
2019-07-08 09:19:27 +0800

    test

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World  ¸Ä¶¯1");
 	}
 
 }
